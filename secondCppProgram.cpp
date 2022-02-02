1d46e43ccf7144daca5994afe71bc7af58a4989b
Jrod7938 <jrod7938@gmail.com>
2022-02-02 15:47:09 -0500

    second c++ program, timestamp: 15:46 20220202

---
+++ secondCppProgram.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    // declare variables
+    int a = 5; 
+    int b = 3;
+    int result;
+    a = a + 1;
+    result = a - b;
+    // print result
+    std::cout << result;
+    //terminate program
+    return 0;
+}
