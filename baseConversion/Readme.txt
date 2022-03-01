400a9405a4deeb39bedf3a1064f51c530641a5ca
Jrod7938 <jrod7938@gmail.com>
2022-03-01 09:26:33 -0500

    Completed Base Conversion C++ Program

---
+++ baseConversion/Readme.txt
@@ -0,0 +1,9 @@
+How to run baseConversion.cpp by Jancarlos Rodriguez
+
+1. Open terminal
+2. navigate to the baseConversion.cpp file
+3. run: g++ baseConversion.cpp -o baseConversion.out
+4. run: ./baseConversion.out
+5. input the number you wish to convert (Currently only works for base 1-9)
+6. input the base of the number
+7. press enter
--- baseConversion/baseConversion.cpp
+++ baseConversion/baseConversion.cpp
@@ -1,69 +1,85 @@
+/* 
+    Jancarlos Rodriguez
+    20220301
+    Class Assignment II - Base Conversion
+    Implemented an algorithm that converts from base 1-9 to base 10
+*/
+
 #include <iostream>
-#include <map>
+#include <cmath>
 
 using namespace std;
 
+// Converts a number from base 1-9 to base 10
 int baseConversion(int number, int base, int numDigits){
     int sum = 0; // Running Total
 
+    // Create a multidimentional array
+    int conversionArray[10][2] = {
+        { '0', 0 },
+        { '1', 1 },
+        { '2', 2 },
+        { '3', 3 },
+        { '4', 4 },
+        { '5', 5 },
+        { '6', 6 },
+        { '7', 7 },
+        { '8', 8 },
+        { '9', 9 },
+    };
 
     string num = to_string(number);
 
+/*
+    For every digit in our number
+        For every row in our conversionArray
+            For every column in our conversion array
+                check if the digit is in the conversion array column 1
+                if it is
+                    multiply its column 2 value by the base raised to the power of the digit place
+                    add it to the sum
+*/
     for (int i = 0; i < numDigits; i++){
-
+        // cout << "First loop i: " << i << endl;
+        for (int j = 0; j < 10; j++){
+            // cout << "Second loop j: " << j << endl;
+            for (int k = 0; k < 1; k++){
+                // cout << "Third loop k: " << k << endl;
+                // cout << "Num[i] = " << num[i] << " ConversionArray[j][0] = " << conversionArray[j][0] << endl;
+                if (num[i] == conversionArray[j][0]){
+                    // cout << conversionArray[j][1] << endl;
+                    sum += conversionArray[j][1] * pow(base, numDigits - i - 1);
+                    // cout << sum << endl;
+                }
+            }
+        }
     }
-
-    
     
     return sum;
 }
 
+
+// Function to count the number of digits in a number
+int digitCounter(int number, int baseTen){
+    int num = number;
+    int digits = 0;
+    
+    while(num){
+        ++digits;
+        num /= baseTen;
+    }
+
+    return digits;
+}
+
 int main(){
+    // Constant variables
     const int baseTen = 10;
 
+    // Int variables
     int num,     // Number to be converted
         n = 0,   // Number of Digits in num
         b;       // Base of the number
-
-    // Create a multidimentional array just like a dictionary
-    map<char, int> my_conversions = {
-    { '0', 0 },
-    { '1', 1 },
-    { '2', 2 },
-    { '3', 3 },
-    { '4', 4 },
-    { '5', 5 },
-    { '6', 6 },
-    { '7', 7 },
-    { '8', 8 },
-    { '9', 9 },
-    { 'A', 10 },
-    { 'B', 11 },
-    { 'C', 12 },
-    { 'D', 13 },
-    { 'E', 14 },
-    { 'F', 15 },
-    { 'G', 16 },
-    { 'H', 17 },
-    { 'I', 18 },
-    { 'J', 19 },
-    { 'K', 20 },
-    { 'L', 21 },
-    { 'M', 22 },
-    { 'N', 23 },
-    { 'O', 24 },
-    { 'P', 25 },
-    { 'Q', 26 },
-    { 'R', 27 },
-    { 'S', 28 },
-    { 'T', 29 },
-    { 'U', 30 },
-    { 'V', 31 },
-    { 'W', 32 },
-    { 'X', 33 },
-    { 'Y', 34 },
-    { 'Z', 35 },
-    };
 
     cout << "Input Number: ";
     cin >> num;
@@ -71,16 +87,14 @@ int main(){
     cout << "Input Base: ";
     cin >> b;
 
-    // Calculates the number of digits in the variable num, and stores it in n
-    while(num){
-        ++n;
-        num /= baseTen;
-    }
+    // Calculates the number of digits in the variable num
+    n = digitCounter(num, baseTen);
 
-    cout << n;
+    //cout << "num: " << num << endl;
+    //cout << "digits: " << n << endl;
+    //cout << "base: " << b << endl;
 
-    baseConversion(num, b, n);
-
+    cout << num << " to base 10 is: " << baseConversion(num, b, n);
 
     return 0;
 }
