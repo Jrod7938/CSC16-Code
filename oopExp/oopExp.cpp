4e2da20acbabbd350dfaf7a89af8aaa29cee52f2
Jrod7938 <jrod7938@gmail.com>
2022-03-08 19:56:33 -0500

    Updated oopExp for class 20220308

--- oopExp/oopExp.cpp
+++ oopExp/oopExp.cpp
@@ -1,6 +1,6 @@
 /*
     Jancarlos Rodriguez
-    20220307
+    20220308
     Object Oriented Programming Example
     Simple program that shows the use of OOP
 */
@@ -15,10 +15,9 @@ class MyClass{
         int getData();
         // Mutator
         void setData(int data);
-        /* inline example
         inline int myFastFunc(){
             return 1+1;
-        }*/
+        };
     private:
         int data;
 
@@ -37,6 +36,22 @@ void MyClass::setData(int data){
     }
 }
 
+ostream &operator << (ostream &out, MyClass &mc){
+    out << mc.getData() << endl;
+    return out;
+}
+
+istream &operator >> (istream &in, MyClass &mc){
+    int data;
+    in >> data;
+    mc.setData(data);
+    return in;
+}
+
+bool operator<(MyClass &mc1, MyClass &mc2){
+    return mc1.getData() < mc2.getData();
+}
+
 int main(){
     MyClass mc;
 
@@ -47,6 +62,27 @@ int main(){
     mc.setData(50);
 
     cout << mc.getData() << endl;
+
+    cout << mc << endl;
+
+    cout << "Please enter data" << endl;
+    cin >> mc;
+    cout << mc;
+    MyClass mc2;
+
+    // mc2 value set to 27
+    mc2.setData(27);
+
+    /*  If mc is smaller than mc2
+            print mc is smaller than mc2
+        else
+            print mc is bigger than mc2
+    */        
+    if(mc<mc2){
+        cout << "mc is smaller than mc2" << endl;
+    } else {
+        cout << "mc is bigger than or equal to mc2" << endl;
+    }
 
     return 0;
 }
